d7814a58afe589743c39998c68dfb746af4233ec
miriamdurazo <md13707@my.bristol.ac.uk>
2023-02-13 22:29:42 +0000

    added an event listener to check whether the user answer matches the movie info obtained from the API

---
+++ assets/movieQuiz.js
@@ -0,0 +1,32 @@
+let movieTitles = ["Fight club", "Kill bill", "Trainspotting", "Forrest Gump", "Inglorious basterds", "Back to the future", "Ghostbusters", "Inception"]
+let movieTitle = movieTitles[Math.floor(Math.random() * movieTitles.length)];
+const queryURL = "https://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy";
+
+$(window).on("load",  function () {
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+
+    console.log(response);
+    
+    console.log(response.Actors);
+    let actorsClueEl = $('<div>').text(response.Actors).addClass('box');
+    $('#clue-box').append(actorsClueEl);
+    })}
+);
+
+
+$('#submit-movie').on("submit", function (event) {
+    event.preventDefault();
+    let inputMovie = $('#input-movie').val();
+     
+    if (inputMovie.toLowerCase() === movieTitle.toLowerCase()) {
+        console.log(`you got it right, it is ${movieTitle}`);
+    } else {
+        console.log("i don't know what's going on"),
+        console.log(`it was ${movieTitle}`);
+    }
+    inputMovie.val('');
+})
+
